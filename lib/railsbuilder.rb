02bda38a829af172fe33c0683bf825d57d424545
James Robertson <james@ralina.(none)>
2014-03-10 23:42:53 +0000

    feature: Can now read and generate a controller + view

--- lib/railsbuilder.rb
+++ lib/railsbuilder.rb
@@ -8,40 +8,72 @@ require 'lineparser'
 
 
 class RailsBuilder
+  
+  attr_reader :to_a
 
   def initialize(filepath=nil)
 
     buffer = File.read filepath if filepath
 
     patterns = [
+      [:root, 'app_path: :app_path', :app_path],
       [:root, 'app: :app', :app],
       [:root, 'resources: :resources', :resources],
       [:root, ':resource', :resource],
         [:resource, 'model', :model],
           [:model, ':class_name', :model_class],
+        [:resource, 'controller + views', :resource_cv],
+          [:resource_cv, /(\w+)\s+[av]{1,2}/, :resource_cv_av],
       [:all, /#/]
     ]
 
-    @h = parse(patterns, buffer)
+    @to_a = @a = parse(patterns, buffer)
   end
 
   def build()
 
-    cols = @h[:root].select {|x| x[1].has_key? ':app' }
+    cols = @a[:root].select {|x| x[1].has_key? ':app' }
     @app = cols[0][1][':app']
 
     unless File.exists? @app then
       command = 'rails new ' + @app
       puts ":: preparing to execute shell command: `#{command}`"
       puts 'Are you sure you want to build a new app? (Y/n)'
-      r = $stdin.getch
 
-      if r[/y|\r/i] then
-        IO.popen(command).each_line {|x| print "", x}
-      else
-        puts 'Abort'
+      shell command
+    else
+      Dir.chdir @app
+    end
+
+    # select the :resource records
+
+    @a[:root].select{|x| x[1][":resource"]}.each do |raw_resource|
+
+      resource_child = raw_resource[3][0][3]
+      resource = raw_resource[1][":resource"]
+
+      case resource_child[0][0]
+
+        when :model
+          puts "it's a model"
+        when :resource_cv
+
+          # fetch the action name
+          action = resource_child[0][1][:captures][0]
+          page = action + '.html.erb'
+      
+          unless File.exists? File.join('app','views', resource, page) then
+
+            command = "rails generate controller %s %s" % [resource, action]
+            puts ":: preparing to execute shell command: `#{command}`"
+            puts 'Are you sure you want to generate a controller action? (Y/n)'
+
+            shell command
+          end
+
       end
     end
+
     @h
   end
 
@@ -78,6 +110,14 @@ EOF
     a.group_by(&:first)
   end
 
+  def shell(command)
+
+    if $stdin.getch[/y|\r/i] then
+      IO.popen(command).each_line {|x| print "", x}
+    else
+      puts 'Abort'
+    end
+  end
 end
 
 =begin
